fa334603525f39491d5524e6cc6735bb213de06b
Zolics <153197257+Zolicsiz@users.noreply.github.com>
2024-04-15 19:49:48 +0300

    Add files via upload

---
+++ EventHandlers.cs
@@ -0,0 +1,39 @@
+﻿using Exiled.API.Features;
+using MEC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hud
+{
+    public class EventHandlers
+    {
+        public CoroutineHandle coroutine = new CoroutineHandle();
+        public IEnumerator<float> Handle()
+        {
+            while(true) 
+            {
+                foreach(Player pl in Player.List)
+                {
+                    if(pl.Role.Team != PlayerRoles.Team.Dead && pl.Role.Type != PlayerRoles.RoleTypeId.Filmmaker)
+                    {
+                        pl.ShowHint($"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n<align=\"left\"><color={pl.Role.Color.ToHex()}>ㅤㅤ{pl.DisplayNickname.Replace("*", "")}</color></align>", 1.4f);
+                    }
+                    
+                }
+                yield return Timing.WaitForSeconds(1f);
+            }
+            
+        }
+        public void WaitingForPlayers()
+        {
+            Timing.KillCoroutines(coroutine);
+        }
+        public void OnRoundStarted()
+        {
+            coroutine = Timing.RunCoroutine(Handle());
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hud
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin plugin;
+        public EventHandlers eventHandlers;
+        public override string Author => "Zolics";
+        public override string Prefix => "hud";
+        public override string Name => "Hud";
+        public override void OnEnabled()
+        {
+            
+            eventHandlers = new EventHandlers();
+            Exiled.Events.Handlers.Server.RoundStarted += eventHandlers.OnRoundStarted;
+            Exiled.Events.Handlers.Server.WaitingForPlayers += eventHandlers.WaitingForPlayers;
+            plugin = this;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            eventHandlers = null;
+            plugin = null;
+            Exiled.Events.Handlers.Server.RoundStarted -= eventHandlers.OnRoundStarted;
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= eventHandlers.WaitingForPlayers;
+            
+            base.OnDisabled();
+        }
+        
+    }
+}
